991f7c82d9337876540892d57e98e14ef0dc7bbe
moniishms <moniishms@gmail.com>
2026-04-07 23:31:10 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ import streamlit as st
 import pandas as pd
 
 st.title("Excel File Upload and Viewer")
-st.write("Write the number of files u want to view")
+num=st.number_input("Enter the number of files to upload",min_value=1, max_value=5,step=1)
 
-uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
+uploaded_files = []
+data_frames=[]
+for i in range(num):
+    uploaded_file = st.file_uploader(f"Upload Excel file {i+1}", type=["xlsx","csv"], key=f"file_{i}")
+    if uploaded_file is not None:
+        uploaded_files.append(uploaded_file)
 
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
+for files in uploaded_files:
+    if files.name.endswith(".xlsx"):
+        df = pd.read_excel(files)
+    elif files.name.endswith(".csv"):
+        df = pd.read_csv(files)
+    data_frames.append(df)
 
-    st.subheader("Dataset Preview")
-    st.dataframe(df)
-
-    st.subheader("Dataset Info")
-    st.write("Rows:", df.shape[0])
-    st.write("Columns:", df.shape[1])
-
-
+if data_frames:
+    merge_type = st.selectbox("Select merge type", ["Row-wise", "Column-wise"])
+    if merge_type == "Row-wise":
+        merged_df = pd.concat(data_frames, ignore_index=True)
+    else:
+        merged_df = pd.concat(data_frames, axis=1)
+    st.write("Merged DataFrame:")
+    st.dataframe(merged_df.head(15))
     
