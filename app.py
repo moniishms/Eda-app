060e9a7a6ed7c55c01e5100e6cc59ba10512470b
moniishms <moniishms@gmail.com>
2026-04-07 09:44:51 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.title("Excel File Upload and Viewer")
+st.write("Write the number of files u want to view")
 
-# Upload Excel file
 uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
 
 if uploaded_file is not None:
-    # Read Excel file
     df = pd.read_excel(uploaded_file)
 
     st.subheader("Dataset Preview")
